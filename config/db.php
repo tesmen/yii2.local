60ae6389ef99b971df880adaeaba883449c664ab
Serge <tezmo@mail.ru>
2017-01-29 00:24:43 +0300

    Update db.php
    DB

--- config/db.php
+++ config/db.php
@@ -1,9 +1,17 @@
 <?php
+//
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+//    'username' => 'root',
+//    'password' => '',
+//    'charset' => 'utf8',
+//];
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=test_database',
+    'username'=>'test_user',
+    'password'=>'qwerty',
+    'charset'=>'UTF8',
 ];
