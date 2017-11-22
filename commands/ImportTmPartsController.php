1713764c633c7458e749371ce92aeeff1b3e04e2
tesmen <tezmo@mail.ru>
2017-11-22 16:59:41 +0300

    ImportTmPartsController

--- commands/ImportTmPartsController.php
+++ commands/ImportTmPartsController.php
@@ -3,15 +3,18 @@
 namespace app\commands;
 
 use app\entity\TmPart;
+use app\traits\ConsoleParamsTrait;
 use yii\console\Controller;
 
 class ImportTmPartsController extends Controller
 {
+    use ConsoleParamsTrait;
+
     public function actionIndex($filename)
     {
         $data = $this->parseCsvFile($filename);
         $dupes = 0;
-
+        $saved = 0;
 
         foreach ($data as $row) {
             if (empty($row[7])) {
@@ -33,9 +36,14 @@ class ImportTmPartsController extends Controller
             $rec->obozn = mb_strtolower($row[6]);
             $rec->raw_name = $detailName;
             $rec->save();
+
+            $saved++;
         }
 
-        var_export('Dupes:' . $dupes);
+        $all = sizeof( $data);
+        $this->writeln("Dupes: $dupes");
+        $this->writeln("imported: $saved");
+        $this->writeln("all: $all");
     }
 
     public function parseCsvFile($file)
